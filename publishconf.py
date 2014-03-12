fd6ac5144ca1c80b85ce355c544c5aeb9091224e
ocefpaf <ocefpaf@gmail.com>
2014-03-12 16:37:56 -0300

    Changed SITEURL.

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://ocefpaf.github.io'
+SITEURL = 'http://ocefpaf.github.io/python4oceanographers'
 
 DELETE_OUTPUT_DIRECTORY = True
 
